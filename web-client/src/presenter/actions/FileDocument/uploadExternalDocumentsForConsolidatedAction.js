6767bdf3223bc6ea84c4a9c0d09ce732e48f9045
William Bagayoko <wbagayoko@gmail.com>
2020-02-05 14:15:17 -0600

    Bug: filter for consolidated submission

--- web-client/src/presenter/actions/FileDocument/uploadExternalDocumentsForConsolidatedAction.js
+++ web-client/src/presenter/actions/FileDocument/uploadExternalDocumentsForConsolidatedAction.js
@@ -65,7 +65,9 @@ export const uploadExternalDocumentsForConsolidatedAction = async ({
 
   const getPendingDocumentsForCase = caseDetail =>
     caseDetail.documents.filter(
-      document => document.processingStatus === 'pending',
+      document =>
+        document.processingStatus === 'pending' &&
+        document.isFileAttached !== false,
     );
 
   const pendingDocuments = [];
